910f8c857ca8cf310899fa958077df0026165750
Hardik Saini <lamardavisdog@gmail.com>
2026-04-20 09:40:16 -0400

    Remove mobile Flutter project
    Delete the entire mobile/ Flutter client and its platform-specific files. This removes the Android and iOS projects, Dart source (lib/), project configs, docs, and metadata under mobile/, effectively removing the mobile app module from the repository. Restore from Git history if this was removed accidentally.

--- mobile/android/app/src/main/kotlin/site/tasktrail/tasktrail_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package site.tasktrail.tasktrail_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- mobile/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-import 'src/app/tasktrail_mobile_app.dart';
-import 'src/core/config/app_config.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(TaskTrailMobileApp(config: AppConfig.fromEnvironment()));
-}
